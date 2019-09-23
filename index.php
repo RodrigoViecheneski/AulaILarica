aaa57853104f5b9b66119a07929e5f32c43054c6
Rodrigo Viecheneski <rodrigoviecheneski@gmail.com>
2019-09-23 14:49:51 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ILarica - Fast Food</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	
+</head>
+<body>
+	<header>
+		<img src="img/i larica .png" alt="Logo ILarica">
+	
+
+	<nav class="menu_opcoes">
+		<ul>
+			<li><a href="#">Sua Conta</a></li>
+			<li><a href="#">Lista I Food</a></li>
+			<li><a href="#">Cartão Fidelidade</a></li>
+			<li><a href="sobre.php">Sobre</a></li>
+			<li><a href="#">Ajuda</a></li>
+		</ul>
+	</nav>
+	</header>
+
+	<div class="container destaque">
+		<section class="busca">
+			<h2>Buscar Pedido</h2>
+			<form>
+				<input type="search">
+				<button>Buscar</button>
+			</form>
+		</section>
+		<!--Fim da busca-->
+
+		<section class="menu_cardapio">
+			
+			<h2>Cardápio</h2>
+			<nav>
+				<ul>
+					<li><a href="#">X-ILarica</a></li>
+					<li><a href="#">Hot Dog-ILarica</a></li>
+					<li><a href="#">Pizza-ILarica</a></li>
+					<li><a href="#">Porções-ILarica</a></li>
+					<li><a href="#">Árabe-ILarica</a></li>
+					<li><a href="#">Calzones-ILarica</a></li>
+				</ul>
+			</nav>
+		</section>
+		<!--Fim Cardápio-->
+
+		<section class="destaque">
+			<figure class="promo">
+				<img src="img/35.png" alt="X-ILarica Mais gostoso do momento">
+			</figure>
+		</section>
+		<!--Fim destaque-->
+		</div>
+
+
+	<div class= "teste">
+	<section class= "testeSessao">
+	<figure>
+		<img src="img/35.png" alt="Imagem hamburger">
+	</figure>
+	</section>
+		<section class="testeLink">
+			<a href="http://www.google.com">Acessar qualquer coisa!!!!</a>
+		</section>
+	</div>
+</body>
+</html>
